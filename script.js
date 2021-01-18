d78472eaeb43d666aa67021d3dcaffd2add7822a
João Vinicius da Costa <joaoviniciusdacosta800@gmail.com>
2021-01-18 20:52:09 +0000

    beta2

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function verificar(){
   p2.innerHTML=`Descrição: ${descricao}`;
 
   if(email == "" || descricao == ""){
-    card.innerHTML=`
+    card.innerHTML =`
             <p class="text-danger text-center card-text">Campo vazio, por favor verifique todos os campos!</p>`
   }
   else{
-      card.innerHTML=`
+      card.innerHTML =`
               <p class="text-success text-center card-text">Dados enviados!</p>`
   }
 }
